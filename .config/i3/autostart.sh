563f95cc1a673a44522af64d8bf59f1a1460b330
Julius Quasebarth <feldrandstudios@gmail.com>
2017-01-14 12:28:18 +0100

    Add new modes

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-xrandr --output DP1 --auto --right-of LVDS1 --rotate left &
+xrandr --output DP1 --auto --right-of LVDS1 &
 nitrogen --restore &
 st -t scratchpad &
 xbacklight -set 50 &
 amixer sset Master 20% &
 dunst &
 nm-applet &
-compton &
+compton -f -D 3 &
 setxkbmap us &
 echo "US" > ~/.config/i3status/keyboardlayout
